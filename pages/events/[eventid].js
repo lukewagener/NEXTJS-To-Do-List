ec17ece4481de43b92858d8d8e90aa82dc2a37aa
Sesh <lukewagener1@gmail.com>
2023-01-13 11:43:26 -0600

    added icons

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,3 +1,9 @@
 function SelectedEventPage() {
-    return <div><h1>Details on a selected event.</h1></div>
+  return (
+    <div>
+      <h1>Details on a selected event.</h1>
+    </div>
+  );
 }
+
+export default SelectedEventPage;
